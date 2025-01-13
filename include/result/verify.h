e9757df3e04585741bdb8f5e5759054730c51a0b
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2025-01-13 16:51:17 -0700

    Add verify macros with a casted error type

--- include/result/verify.h
+++ include/result/verify.h
@@ -21,3 +21,26 @@ auto r = rhs; \
 if(!r.has_value()) return r.error(); \
 lhs = *r; \
 } 
+
+
+
+#define RESULT_CAST_VERIFY(fn, err_type) \
+{ \
+auto r = fn; \
+if(!r.has_value()) return static_cast<err_type>(r.error()); \
+}
+
+
+#define RESULT_CAST_TRY_MOVE(lhs, rhs, err_type) \
+{ \
+auto r = rhs; \
+if(!r.has_value()) return static_cast<err_type>(r.error()); \
+lhs = *std::move(r); \
+} 
+
+#define RESULT_CAST_TRY_COPY(lhs, rhs, err_type) \
+{ \
+auto r = rhs; \
+if(!r.has_value()) return static_cast<err_type>(r.error()); \
+lhs = *r; \
+} 
