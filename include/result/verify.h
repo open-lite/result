d1c32139084d8575cdf26a547fb106948a25d0f5
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2025-01-13 18:35:22 -0700

    Add unscoped variants of verify macros

--- include/result/verify.h
+++ include/result/verify.h
@@ -1,26 +1,29 @@
 #pragma once
 
+#define RESULT_VERIFY_UNSCOPED(fn, var) \
+auto var = fn; \
+if(!var.has_value()) return var.error();
+
+
+#define RESULT_TRY_MOVE_UNSCOPED(lhs, rhs, var) \
+RESULT_VERIFY_UNSCOPED(rhs, var) \
+lhs = *std::move(var);
+
+#define RESULT_TRY_COPY_UNSCOPED(lhs, rhs, var) \
+RESULT_VERIFY_UNSCOPED(rhs, var) \
+lhs = *var;
+
+
 
 #define RESULT_VERIFY(fn) \
-{ \
-auto r = fn; \
-if(!r.has_value()) return r.error(); \
-}
+{ RESULT_VERIFY_UNSCOPED(fn, r) }
 
 
 #define RESULT_TRY_MOVE(lhs, rhs) \
-{ \
-auto r = rhs; \
-if(!r.has_value()) return r.error(); \
-lhs = *std::move(r); \
-} 
+{ RESULT_TRY_MOVE_UNSCOPED(lhs, rhs, r) } 
 
 #define RESULT_TRY_COPY(lhs, rhs) \
-{ \
-auto r = rhs; \
-if(!r.has_value()) return r.error(); \
-lhs = *r; \
-} 
+{ RESULT_TRY_COPY_UNSCOPED(lhs, rhs, r) } 
 
 
 
